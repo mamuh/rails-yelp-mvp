21a2ee8fa2c1ebb0ff7925f70d58b6c397ea0177
Mariana Caramori <musardo@mail.com>
2020-05-27 14:29:34 -0300

    app is functional, restaurants and reviews work

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,13 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+puts "creating..."
+5.times do
+  restaurant = Restaurant.create(
+    name: Faker::Restaurant.name,
+    address: Faker::Address.street_address,
+    category: ["chinese", "italian", "japanese", "french", "belgian"].sample,
+    phone_number: Faker::Address.zip
+    )
+  puts "created #{restaurant}"
+end
